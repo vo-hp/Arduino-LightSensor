1a5b2ff2562c18de95c028e76cb09de3d97aac8d
vo-hp <vohuuphuc1911@gmail.com>
2023-08-30 21:53:21 +0700

    Update Arduino-LightSensor.ino

--- Arduino-LightSensor.ino
+++ Arduino-LightSensor.ino
@@ -11,6 +11,17 @@ const int MIN_VALUE = 0;
 const int EXPECTED_VALUE = 40;
 int ledValue = 0;
 int ledPin = 6;
+bool slowUpdate = false;
+int preValue[4] = { 0, 0, 0, 0 };
+void pushValue(int value) {
+  preValue[0] = preValue[1];
+  preValue[1] = preValue[2];
+  preValue[2] = preValue[3];
+  preValue[3] = value;
+}
+bool checkBlink() {
+  return preValue[0] == 0 or preValue[1] == 0 or  preValue[2] == 0 or preValue[3] == 0;
+}
 
 void setup() {
   Serial.begin(115200);
@@ -30,21 +41,37 @@ void loop() {
     Serial.print(lux);
     Serial.print(" lx    ");
     if (lux > EXPECTED_VALUE) {
-      int minusValue = map(lux, 40, 200, 0, 125);
+      int minusValue = map(lux, 40, 200, 0, 62);
+      if (minusValue > 10) {
+        minusValue = 10;
+      }
+      if (slowUpdate) {
+        minusValue = 1;
+      }
       ledValue -= minusValue;
-      ledValue -= 1;
+      // ledValue -= 1;
+      Serial.print("  - " + String(minusValue) + "   ");
     }
     if (lux < EXPECTED_VALUE) {
-      int plusValue = map(lux, 0, 40, 0, 62);
+      int plusValue = map(lux, 40, 0, 0, 62);
+      if (plusValue > 10) {
+        plusValue = 10;
+      }
+      if (slowUpdate) {
+        plusValue = 1;
+      }
       ledValue += plusValue;
-      ledValue += 1;
+      // ledValue += 1;
+      Serial.print("  + " + String(plusValue) + "   ");
     }
-    if ( ledValue <= 0) {
+    if (ledValue <= 0) {
       ledValue = 0;
     }
-    if ( ledValue >= 255) {
+    if (ledValue >= 255) {
       ledValue = 255;
     }
+    pushValue(ledValue);
+    slowUpdate = checkBlink();
     analogWrite(ledPin, ledValue);
     Serial.println(ledValue);
   }
